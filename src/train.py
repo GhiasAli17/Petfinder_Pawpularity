35c7a94f2ce96a48fa37ad5f6e59a061f7686890
ghiasali17 <ghiasali17@gmail.com>
2025-09-29 12:08:29 +0900

    unused mixup removed

--- src/train.py
+++ src/train.py
@@ -2,7 +2,7 @@ import torch
 from tqdm import tqdm
 from sklearn.metrics import root_mean_squared_error as sklearn_rmse
 from utils.helpers import MetricMonitor
-from src.data import mixup_data, mixup_criterion # Import mixup functions from data.py
+
 
 def train_fn(train_loader, model, criterion, optimizer, epoch, params, scheduler=None):
     """
@@ -16,30 +16,16 @@ def train_fn(train_loader, model, criterion, optimizer, epoch, params, scheduler
     all_train_targets = []
 
     for i, (images, dense, target) in enumerate(stream, start=1):
-        if params['mixup']:
-            images, dense, target_a, target_b, lam = mixup_data(images, dense, target.view(-1, 1), params)
-            
-            images = images.to(params['device'], dtype=torch.float)
-            dense = dense.to(params['device'], dtype=torch.float)
-            target_a = target_a.to(params['device'], dtype=torch.float)
-            target_b = target_b.to(params['device'], dtype=torch.float)
-            
-            output = model(images, dense)
-            loss = mixup_criterion(criterion, output, target_a, target_b, lam)
-            
-         
-            all_train_preds.extend(torch.sigmoid(output).detach().cpu().numpy().flatten() * 100)
-            all_train_targets.extend(target_a.detach().cpu().numpy().flatten() * 100) 
-        else:
-            images = images.to(params['device'], non_blocking=True)
-            dense = dense.to(params['device'], non_blocking=True)
-            target = target.to(params['device'], non_blocking=True).float().view(-1, 1)
-            
-            output = model(images, dense)
-            loss = criterion(output, target)
-            
-            all_train_preds.extend(torch.sigmoid(output).detach().cpu().numpy().flatten() * 100)
-            all_train_targets.extend(target.detach().cpu().numpy().flatten() * 100)
+       
+        images = images.to(params['device'], non_blocking=True)
+        dense = dense.to(params['device'], non_blocking=True)
+        target = target.to(params['device'], non_blocking=True).float().view(-1, 1)
+        
+        output = model(images, dense)
+        loss = criterion(output, target)
+        
+        all_train_preds.extend(torch.sigmoid(output).detach().cpu().numpy().flatten() * 100)
+        all_train_targets.extend(target.detach().cpu().numpy().flatten() * 100)
 
 
         metric_monitor.update('Loss', loss.item())
